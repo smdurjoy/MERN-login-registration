6ebbf4ee568b4964b183a1fda4ee3b56f2414f08
smdurjoy <smdurjoy963@gmail.com>
2020-12-13 18:21:29 +0600

    Add auth middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const auth = require('../middleware/auth')
 
 const router = express.Router()
 
@@ -31,6 +33,63 @@ router.post('/signup', async (request, response) => {
 
         const savedUser = await newUser.save()
         response.json(savedUser)
+    } catch (error) {
+        response.status(500).json({error: error.message})
+    }
+})
+
+router.post('/login', async (request, response) => {
+    try {
+        const { email, password } = request.body
+
+        if(!email || !password)
+            return response.status(400).json({msg: "Not all fields have been entered !"})
+
+        const user = await User.findOne({email: email})
+        if(!user)
+            return response.status(400).json({msg: "No account with this email have been found !"})
+        
+        const isMatched = await bcrypt.compare(password, user.password)
+        if(!isMatched)
+            return response.status(400).json({msg: "Invalid Credentials !"})
+        
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+        response.json({
+            token,
+            user: {
+                id: user._id,
+                email: user.email,
+                name: user.name
+            }
+        })
+
+    } catch (error) {
+        response.status(500).json({error: error.message})
+    }
+})
+
+router.delete('/delete', auth, async (req, res) => {
+    try {
+        const deleteUser = await User.findById(req.user)
+        res.json(deleteUser)
+    } catch (error) {
+        response.status(500).json({error: error.message})
+    }
+})
+
+router.post('/isTokenValid', async (req, res) => {
+    try {
+        const token = req.header('x-auth-token')
+        if(!token) return res.json(false)
+
+        const varified = jwt.verify(token, process.env.JWT_SECRET)
+        if(!varified) return res.json(false)
+
+        const user = await User.findById(varified.id)
+        if(!user) return res.json(false)
+
+        return res.json(true)
+
     } catch (error) {
         response.status(500).json({error: error.message})
     }
